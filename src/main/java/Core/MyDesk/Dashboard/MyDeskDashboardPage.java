3d001a864541482aeef5b41c1f4c75825ffc4338
nehakottawar <116713308+nehakottawar@users.noreply.github.com>
2025-02-14 18:49:15 +0530

    seventh Commit

--- src/main/java/Core/MyDesk/Dashboard/MyDeskDashboardPage.java
+++ src/main/java/Core/MyDesk/Dashboard/MyDeskDashboardPage.java
@@ -140,34 +140,53 @@ public class MyDeskDashboardPage extends Base_Class {
 	}
 
 	public void VerifyConfigZone() throws IOException {
-		File file = new File(".\\src\\test\\resources\\MyDeskDashboardconfig2");
-		System.out.println("File exists: " + file.exists());
-		ExtentTestManager.getTest().log(Status.PASS, "My Desk Dashboard config2 file is present:" + file.exists());
-		String ZoneUserID = configloader().getProperty("ZoneUserID").trim();
-		String ZoneUserPW = configloader().getProperty("Zonepassword").trim();
-		ExtentTestManager.getTest().log(Status.PASS, "Config file Zone user ID  " + ZoneUserID);
-		ExtentTestManager.getTest().log(Status.PASS, "Zone User PW  " + ZoneUserPW);
+		try {
+			File file = new File(".\\src\\test\\resources\\MyDeskDashboardconfig2");
+			System.out.println("File exists: " + file.exists());
+			ExtentTestManager.getTest().log(Status.PASS, "My Desk Dashboard config2 file is present:" + file.exists());
+			String ZoneUserID = configloader().getProperty("ZoneUserID").trim();
+			String ZoneUserPW = configloader().getProperty("Zonepassword").trim();
+			ExtentTestManager.getTest().log(Status.PASS, "Config file Zone user ID  " + ZoneUserID);
+			ExtentTestManager.getTest().log(Status.PASS, "Zone User PW  " + ZoneUserPW);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
+		}
 	}
 
 	public void VerifyConfigRegion() throws IOException {
-		File file = new File(".\\src\\test\\resources\\MyDeskDashboardconfig2");
-		System.out.println("File exists: " + file.exists());
-		ExtentTestManager.getTest().log(Status.PASS, "My Desk Dashboard config2 file is present:" + file.exists());
-		String RegionUserID = MyDeskDashboardPage.configloader().getProperty("RegionUserID").trim();
-		ExtentTestManager.getTest().log(Status.PASS, "Region User ID from config2 file" + RegionUserID);
+		try {
+			File file = new File(".\\src\\test\\resources\\MyDeskDashboardconfig2");
+			System.out.println("File exists: " + file.exists());
+			ExtentTestManager.getTest().log(Status.PASS, "My Desk Dashboard config2 file is present:" + file.exists());
+			String RegionUserID = MyDeskDashboardPage.configloader().getProperty("RegionUserID").trim();
+			ExtentTestManager.getTest().log(Status.PASS, "Region User ID from config2 file" + RegionUserID);
 
-		String Regionpassword = MyDeskDashboardPage.configloader().getProperty("Regionpassword").trim();
-		ExtentTestManager.getTest().log(Status.PASS, "Region User password config2 file " + Regionpassword);
+			String Regionpassword = MyDeskDashboardPage.configloader().getProperty("Regionpassword").trim();
+			ExtentTestManager.getTest().log(Status.PASS, "Region User password config2 file " + Regionpassword);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
+			
+		}
 	}
 
 	public void VerifyConfigBranch() throws IOException {
-		File file = new File(".\\src\\test\\resources\\MyDeskDashboardconfig2");
-		System.out.println("File exists: " + file.exists());
-		ExtentTestManager.getTest().log(Status.PASS, "My Desk Dashboard config2 file is present:" + file.exists());
-		String Core_BranchUserID = MyDeskDashboardPage.configloader().getProperty("Core_BranchUserID").trim();
-		ExtentTestManager.getTest().log(Status.PASS, "Core Branch User ID config2 file is " + Core_BranchUserID);
-		String Core_Branchpassword = MyDeskDashboardPage.configloader().getProperty("Core_Branchpassword").trim();
-		ExtentTestManager.getTest().log(Status.PASS, "Core Branch User password  " + Core_Branchpassword);
+		try {
+			File file = new File(".\\src\\test\\resources\\MyDeskDashboardconfig2");
+			System.out.println("File exists: " + file.exists());
+			ExtentTestManager.getTest().log(Status.PASS, "My Desk Dashboard config2 file is present:" + file.exists());
+			String Core_BranchUserID = MyDeskDashboardPage.configloader().getProperty("Core_BranchUserID").trim();
+			ExtentTestManager.getTest().log(Status.PASS, "Core Branch User ID config2 file is " + Core_BranchUserID);
+			String Core_Branchpassword = MyDeskDashboardPage.configloader().getProperty("Core_Branchpassword").trim();
+			ExtentTestManager.getTest().log(Status.PASS, "Core Branch User password  " + Core_Branchpassword);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
+		}
 	}
 
 	public void VerifyConfigBCOUser() throws IOException {
@@ -178,121 +197,145 @@ public class MyDeskDashboardPage extends Base_Class {
 	}
 
 	public void SQLQueryZone() throws IOException {
-		String ZoneUserID = configloader().getProperty("ZoneUserID").trim();
-		List<Object> inputParams = Arrays.asList(ZoneUserID, "John Doe", "john.doe@example.com", 9876543210L);
+		try {
+			String ZoneUserID = configloader().getProperty("ZoneUserID").trim();
+			List<Object> inputParams = Arrays.asList(ZoneUserID, "John Doe", "john.doe@example.com", 9876543210L);
 //		List<Object> inputParams = Arrays.asList(null, "John Doe", "john.doe@example.com", 9876543210L);
-		List<Integer> outputTypes = Arrays.asList(Types.VARCHAR, Types.VARCHAR, Types.VARCHAR);
-		List<Object> results = DBUtils.ExecuteAnyOracleSQLStoredProcedure("ZoneUserIDGenerator", inputParams,
-				outputTypes);
+			List<Integer> outputTypes = Arrays.asList(Types.VARCHAR, Types.VARCHAR, Types.VARCHAR);
+			List<Object> results = DBUtils.ExecuteAnyOracleSQLStoredProcedure("ZoneUserIDGenerator", inputParams,
+					outputTypes);
 
-		System.out.println("Generated User ID: " + results.get(0));
-		String Username = (String) results.get(0);
-		System.out.println("Default Password: " + results.get(1));
-		String Password = (String) results.get(1);
-		System.out.println("Status Message: " + results.get(2));
-		String Message = (String) results.get(2);
-		System.out.println(Message);
+			System.out.println("Generated User ID: " + results.get(0));
+			String Username = (String) results.get(0);
+			System.out.println("Default Password: " + results.get(1));
+			String Password = (String) results.get(1);
+			System.out.println("Status Message: " + results.get(2));
+			String Message = (String) results.get(2);
+			System.out.println(Message);
 
-		if (Message != null) { // Check if Message is not null
-			if (Message.contains("null")) {
-				ExtentTestManager.getTest().log(Status.PASS, "Message is null need to create new USER ID ");
-			} else if (Message.contains(Message)) {
-				ExtentTestManager.getTest().log(Status.PASS, "Message from the Data Base" + Message);
+			if (Message != null) { // Check if Message is not null
+				if (Message.contains("null")) {
+					ExtentTestManager.getTest().log(Status.PASS, "Message is null need to create new USER ID ");
+				} else if (Message.contains(Message)) {
+					ExtentTestManager.getTest().log(Status.PASS, "Message from the Data Base" + Message);
+				}
+			} else {
+				System.out.println("Warning: Message is null");
+				SaveConfigFileZone(Username, Password);
+				ExtentTestManager.getTest().log(Status.PASS, "Saved the User Details in Config file");
 			}
-		} else {
-			System.out.println("Warning: Message is null");
-			SaveConfigFileZone(Username, Password);
-			ExtentTestManager.getTest().log(Status.PASS, "Saved the User Details in Config file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
 		}
 	}
 
 	public void SQLQueryRegion() throws IOException {
-		String RegionUserID = MyDeskDashboardPage.configloader().getProperty("RegionUserID").trim();
-		List<Object> inputParams = Arrays.asList(RegionUserID, "John Doe", "john.doe@example.com", 9876543210L);
+		try {
+			String RegionUserID = MyDeskDashboardPage.configloader().getProperty("RegionUserID").trim();
+			List<Object> inputParams = Arrays.asList(RegionUserID, "John Doe", "john.doe@example.com", 9876543210L);
 //		List<Object> inputParams = Arrays.asList(null, "John Doe", "john.doe@example.com", 9876543210L);
-		List<Integer> outputTypes = Arrays.asList(Types.VARCHAR, Types.VARCHAR, Types.VARCHAR);
+			List<Integer> outputTypes = Arrays.asList(Types.VARCHAR, Types.VARCHAR, Types.VARCHAR);
 
-		List<Object> results = DBUtils.ExecuteAnyOracleSQLStoredProcedure("RegionUserIDGenerator", inputParams,
-				outputTypes);
-		System.out.println("Generated User ID: " + results.get(0));
-		String Username = (String) results.get(0);
-		System.out.println("Default Password: " + results.get(1));
-		String Password = (String) results.get(1);
-		System.out.println("Status Message: " + results.get(2));
-		String Message = (String) results.get(2);
-		System.out.println(Message);
+			List<Object> results = DBUtils.ExecuteAnyOracleSQLStoredProcedure("RegionUserIDGenerator", inputParams,
+					outputTypes);
+			System.out.println("Generated User ID: " + results.get(0));
+			String Username = (String) results.get(0);
+			System.out.println("Default Password: " + results.get(1));
+			String Password = (String) results.get(1);
+			System.out.println("Status Message: " + results.get(2));
+			String Message = (String) results.get(2);
+			System.out.println(Message);
 
-		if (Message != null) { // Check if Message is not null
-			if (Message.contains("null")) {
-				ExtentTestManager.getTest().log(Status.PASS, "Message is null need to create new USER ID ");
-			} else if (Message
-					.contains("UserIDFromPropertiesFile exist in acc_users table. Zone user creation aborted.")) {
-				// Handle this case if needed
-				ExtentTestManager.getTest().log(Status.PASS, "Message from the Data Base" + Message);
+			if (Message != null) { // Check if Message is not null
+				if (Message.contains("null")) {
+					ExtentTestManager.getTest().log(Status.PASS, "Message is null need to create new USER ID ");
+				} else if (Message
+						.contains("UserIDFromPropertiesFile exist in acc_users table. Zone user creation aborted.")) {
+					// Handle this case if needed
+					ExtentTestManager.getTest().log(Status.PASS, "Message from the Data Base" + Message);
+				}
+			} else {
+				System.out.println("Warning: Message is null");
+				SaveConfigFileRegion(Username, Password);
+				ExtentTestManager.getTest().log(Status.PASS, "Saved the User Details in Config file");
 			}
-		} else {
-			System.out.println("Warning: Message is null");
-			SaveConfigFileRegion(Username, Password);
-			ExtentTestManager.getTest().log(Status.PASS, "Saved the User Details in Config file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
 		}
 
 	}
 
 	public void SQLQueryBranch() throws IOException {
 
-		String Core_BranchUserID = MyDeskDashboardPage.configloader().getProperty("Core_BranchUserID").trim();
-		List<Object> inputParams = Arrays.asList(Core_BranchUserID, "John Doe", "john.doe@example.com", 9876543210L);
+		try {
+			String Core_BranchUserID = MyDeskDashboardPage.configloader().getProperty("Core_BranchUserID").trim();
+			List<Object> inputParams = Arrays.asList(Core_BranchUserID, "John Doe", "john.doe@example.com", 9876543210L);
 //	List<Object> inputParams = Arrays.asList(null, "John Doe", "john.doe@example.com", 9876543210L);
-		List<Integer> outputTypes = Arrays.asList(Types.VARCHAR, Types.VARCHAR, Types.VARCHAR);
+			List<Integer> outputTypes = Arrays.asList(Types.VARCHAR, Types.VARCHAR, Types.VARCHAR);
 
-		List<Object> results = DBUtils.ExecuteAnyOracleSQLStoredProcedure("Core_BranchUserIDGenerator", inputParams,
-				outputTypes);
-		System.out.println("Generated User ID: " + results.get(0));
-		String Username = (String) results.get(0);
+			List<Object> results = DBUtils.ExecuteAnyOracleSQLStoredProcedure("Core_BranchUserIDGenerator", inputParams,
+					outputTypes);
+			System.out.println("Generated User ID: " + results.get(0));
+			String Username = (String) results.get(0);
 
-		System.out.println("Default Password: " + results.get(1));
-		String Password = (String) results.get(1);
-		System.out.println("Status Message: " + results.get(2));
-		String Message = (String) results.get(2);
-		System.out.println(Message);
-		if (Message != null) { // Check if Message is not null
-			if (Message.contains("null")) {
-				ExtentTestManager.getTest().log(Status.PASS, "Message is null need to create new USER ID ");
-			} else if (Message.contains(Message)) {
-				// Handle this case if needed
-				ExtentTestManager.getTest().log(Status.PASS, "Message from the Data Base" + Message);
+			System.out.println("Default Password: " + results.get(1));
+			String Password = (String) results.get(1);
+			System.out.println("Status Message: " + results.get(2));
+			String Message = (String) results.get(2);
+			System.out.println(Message);
+			if (Message != null) { // Check if Message is not null
+				if (Message.contains("null")) {
+					ExtentTestManager.getTest().log(Status.PASS, "Message is null need to create new USER ID ");
+				} else if (Message.contains(Message)) {
+					// Handle this case if needed
+					ExtentTestManager.getTest().log(Status.PASS, "Message from the Data Base" + Message);
+				}
+			} else {
+				System.out.println("Warning: Message is null");
+				SaveConfigFileBranch(Username, Password);
+				ExtentTestManager.getTest().log(Status.PASS, "Saved the User Details in Config file");
 			}
-		} else {
-			System.out.println("Warning: Message is null");
-			SaveConfigFileBranch(Username, Password);
-			ExtentTestManager.getTest().log(Status.PASS, "Saved the User Details in Config file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
 		}
 
 	}
 
 	public void SQL_BCOUSer() throws IOException {
-		String procedureName = "InsertToMSTEmployeeForCoreMyDeskDashboard";
-		// Input parameters
-		List<Object> inputParams = new ArrayList<>();
-		String Core_BranchUserID = MyDeskDashboardPage.configloader().getProperty("Core_BranchUserID").trim();
-		inputParams.add(Core_BranchUserID); // Example USER_ID
-		// Output parameters
-		List<Integer> outputParamTypes = new ArrayList<>();
-		outputParamTypes.add(Types.VARCHAR); // p_MESSAGE output parameter
+		try {
+			String procedureName = "InsertToMSTEmployeeForCoreMyDeskDashboard";
+			// Input parameters
+			List<Object> inputParams = new ArrayList<>();
+			String Core_BranchUserID = MyDeskDashboardPage.configloader().getProperty("Core_BranchUserID").trim();
+			inputParams.add(Core_BranchUserID); // Example USER_ID
+			// Output parameters
+			List<Integer> outputParamTypes = new ArrayList<>();
+			outputParamTypes.add(Types.VARCHAR); // p_MESSAGE output parameter
 
-		// Execute stored procedure
-		List<Object> output = DBUtils.InsertToMSTEmployeeForCoreMyDeskDashboard(procedureName, inputParams,
-				outputParamTypes);
-		ExtentTestManager.getTest().log(Status.PASS,
-				"Insert To MST Employee For Core My Desk Dashboard procedure done ");
-		// Print output
-		if (!output.isEmpty()) {
-			System.out.println("Stored Procedure Output: " + output.get(0));
+			// Execute stored procedure
+			List<Object> output = DBUtils.InsertToMSTEmployeeForCoreMyDeskDashboard(procedureName, inputParams,
+					outputParamTypes);
+			ExtentTestManager.getTest().log(Status.PASS,
+					"Insert To MST Employee For Core My Desk Dashboard procedure done ");
+			// Print output
+			if (!output.isEmpty()) {
+				System.out.println("Stored Procedure Output: " + output.get(0));
 
-			ExtentTestManager.getTest().log(Status.PASS, "Stored Procedure Output: " + output.get(0));
-		} else {
-			System.out.println("No output received.");
-			ExtentTestManager.getTest().log(Status.FAIL, "Stored Procedure Output: " + output.get(0));
+				ExtentTestManager.getTest().log(Status.PASS, "Stored Procedure Output: " + output.get(0));
+			} else {
+				System.out.println("No output received.");
+				ExtentTestManager.getTest().log(Status.FAIL, "Stored Procedure Output: " + output.get(0));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			ExtentTestManager.getTest().log(Status.FAIL, e.getMessage());	
 		}
 	}
 
